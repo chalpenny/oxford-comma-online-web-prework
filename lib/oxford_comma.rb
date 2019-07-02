4a2f6895f24bce991f3a44fa830d0c784de05521
elegant-intelligence-1985 <cp.halpenny@gmail.com>
2019-07-02 04:27:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
-  #convert an array into a string, using commas to separate
   #push the word "and" in the posiition second to last
-  array.join(", ")
+    array.insert(-2, "and")
+  #convert an array into a string, using commas to separate 
+  # ONLY IF there are more than 3 elements in the array
+  if array.count > 2  
+    array.join(", ")
+  else
+    do nothing
+  end
 end
